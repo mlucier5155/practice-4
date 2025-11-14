158c4c2883e5df05baa68cd848971b1d105ef1b5
mlucier5155 <mlucier7527@floridapoly.edu>
2025-11-14 23:30:32 +0000

    computer choice has been added

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
+import random
+
 print("Welcome to Rock-Paper-Scissors!")
-g
-player = input("Enter rock, paper, or scissors: ")
-print("You chose:", player)
+
+computer = random.choice(["rock", "paper", "scissors"])
+print("Computer chose:", computer)
